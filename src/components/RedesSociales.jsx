b369ce9e9906bca122f85bdc75cedda8fb4d6f44
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2024-12-17 20:43:22 -0500

    cambios y mejoras

--- src/components/RedesSociales.jsx
+++ src/components/RedesSociales.jsx
@@ -3,16 +3,16 @@ export const RedesSociales = () => {
     <div className="redes h-[250px] text-center p-14 flex flex-col gap-10">
       <div className="flex flex-row justify-center items-center gap-5">
         <a className="transform hover:scale-125 flex justify-center items-center hover:bg-white hover:duration-300 transition-all" href="https://www.linkedin.com/in/bryan-lee-chiang-arteaga-048452196/" target="_blank">
-          <img src="/linkedin.png" alt="" />
+          <img src="./linkedin.png" alt="" />
         </a>
         <a className="transform hover:scale-125 flex justify-center items-center hover:bg-white hover:duration-300 transition-all" href="https://es-la.facebook.com/" target="_blank">
-          <img src="/fb.png" alt="" />
+          <img src="./fb.png" alt="" />
         </a>
         <a className="transform hover:scale-125 flex justify-center items-center hover:bg-white hover:duration-300 transition-all" href="https://github.com/ChiangArt" target="_blank">
-          <img src="/github.png" alt="" />
+          <img src="./github.png" alt="" />
         </a>
         <a className="transform hover:scale-125 flex justify-center items-center hover:bg-white hover:duration-300 transition-all" href="https://www.instagram.com/bryanchiang_art" target="_blank">
-          <img src="/instagram_15527615.png" alt="" />
+          <img src="./instagram_15527615.png" alt="" />
         </a>
       </div>
       <p className="text-white">&copy; Bryan Chiang 2023</p>
